ca9b5767bf3aca3e368679569d97c65d5944526f
mridulRahman01 <mridulrahaman430@gmail.com>
2024-11-18 18:25:39 +0600

    Adjust banner

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,6 @@
 const Hero = () => {
   return (
-    <div className="relative  border-yellow-50 flex justify-center mt-[-200px] ">
-      <img
-        className="w-[1000px] h-[550px] rounded-lg  "
-        src="/public/banner.jpg"
-        alt="Loading"
-      />
-    </div>
+    <div className="relative bg-banner bg-cover  border-yellow-50 flex justify-center mt-[-200px] mx-auto w-[1000px] h-[550px] rounded-lg   "></div>
   );
 };
 
